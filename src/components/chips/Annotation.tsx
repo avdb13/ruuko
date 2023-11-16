344aac3edf6faa9d8115e009527cd354e1d4e724
mikoto <avdb@keemail.me>
2023-11-16 14:23:32 +0000

    custom emojis

--- src/components/chips/Annotation.tsx
+++ src/components/chips/Annotation.tsx
@@ -1,9 +1,14 @@
 import { MatrixEvent } from "matrix-js-sdk";
+import { useContext } from "react";
+import { ClientContext } from "../../providers/client";
 
 const Annotation = ({ annotation }: { annotation: MatrixEvent }) => {
+  const key = annotation.getContent()["m.relates_to"]?.key;
+  const client = useContext(ClientContext);
+
   return (
-    <div className="inline-flex items-center bg-gray-50 text-gray-600 px-2 ring-4 ring-inset ring-gray-500/10 rounded-md">
-      <p>{annotation.getContent()["m.relates_to"]?.key}</p>
+    <div className="inline-flex items-center bg-gray-50 text-gray-600 px-2 py-1 ring-4 ring-inset ring-gray-500/10 rounded-md">
+      <p>{key?.startsWith("mxc") ? <img src={client.mxcUrlToHttp(key)!} className="h-5" alt={key} /> : key }</p>
     </div>
   )
 }
