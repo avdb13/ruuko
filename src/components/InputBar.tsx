40ec51418e8dab1796808f0f97597b89ef62a60e
mikoto <avdb@keemail.me>
2023-11-29 13:54:42 +0000

    FilePreview component

--- src/components/InputBar.tsx
+++ src/components/InputBar.tsx
@@ -9,6 +9,7 @@ import {
 import { ClientContext } from "../providers/client";
 import FileIcon from "./icons/File";
 import EmojiPicker, { EmojiStyle } from "emoji-picker-react";
+import CrossIcon from "./icons/Cross";
 
 const FilePicker = ({
   files,
@@ -89,36 +90,58 @@ const InputBar = ({ roomId }: { roomId: string }) => {
     setMessage("");
   };
 
+  // {files ? (<div>hello</div>) : null}
   return (
-    <form onSubmit={handleSubmit} className="flex gap-2 sticky h-12 mx-2">
-      <FilePicker files={files} setFiles={setFiles} />
-      <div className="flex bg-green-200 grow rounded-md my-2">
-        <input
-          id="input-panel"
-          className="grow bg-transparent focus:outline-none mx-2"
-          placeholder={`Message ${client.getRoom(roomId)?.name}`}
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-        />
-        <span className="relative flex justify-center items-center basis-8 border-2">
-          <button
-            className="peer active:bg-red-100"
-            onClick={() => setShowEmojis(!showEmojis)}
-          >
-            😺
-          </button>
-          <div className="absolute right-[0%] bottom-[100%] peer-active:translate-y-[10%] peer-active:opacity-0 opacity-100 translate-x-0 duration-300 ease-out">
-            {showEmojis ? (
-              <EmojiPicker
-                skinTonesDisabled={true}
-                previewConfig={{ showPreview: false }}
-              />
-            ) : null}
-          </div>
-        </span>
-      </div>
-    </form>
+    <div className="flex flex-col">
+      {files ? (
+        files.map(file => <FilePreview file={file} />)
+      ) : null}
+      <form onSubmit={handleSubmit} className="flex items-center gap-2 sticky h-12 mx-2 border-2">
+        <FilePicker files={files} setFiles={setFiles} />
+        <div className="flex bg-green-200 grow rounded-md my-2 py-1">
+          <input
+            id="input-panel"
+            className="grow bg-transparent focus:outline-none mx-2"
+            placeholder={`Message ${client.getRoom(roomId)?.name}`}
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+          <span className="relative flex justify-center items-center basis-8">
+            <button className="peer" onClick={() => setShowEmojis(!showEmojis)}>
+              😺
+            </button>
+            <div className="absolute right-[0%] bottom-[100%] peer-active:translate-y-[10%] peer-active:opacity-0 opacity-100 translate-x-0 duration-300 ease-out">
+              {showEmojis ? (
+                <EmojiPicker
+                  skinTonesDisabled={true}
+                  previewConfig={{ showPreview: false }}
+                  onEmojiClick={(e, _) => {
+                    setMessage(message.concat(e.emoji));
+                    setShowEmojis(false);
+                  }}
+                />
+              ) : null}
+            </div>
+          </span>
+        </div>
+      </form>
+    </div>
   );
 };
+
+const FilePreview = ({ file }: { file: File }) => {
+  // figure out how to truncate in the middle later
+  const fileName = file.name.split(".");
+
+  return (
+    <div className="relative box-border flex flex-col justify-center w-48 h-48 border-2 p-2 m-4">
+      <button className="absolute -top-[12px] -right-[12px] text-sm font-bold bg-red-400 rounded-full"><CrossIcon /></button>
+      <div className="flex items-center grow">
+        <img src={URL.createObjectURL(file)} />
+      </div>
+      <p className="truncate">{file.name}</p>
+    </div>
+  )
+}
 
 export default InputBar;
