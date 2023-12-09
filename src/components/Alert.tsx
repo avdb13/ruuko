b1d3b15fa7ada596c37b75f4fe289dadb7af7288
mikoto <avdb@keemail.me>
2023-12-09 14:12:44 +0000

    responsive absolute positioning

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -21,12 +21,12 @@ const Alert = ({
   }
 
   return (
-    <div className="absolute left-[25%] border-b-2 border-gray-300 top-4 shadow-xl w-[300px] flex justify-center items-center bg-white rounded-sm h-fit z-10">
+    <div className="alert absolute border-b-2 border-gray-300 shadow-xl w-[50%] flex justify-center items-center bg-white rounded-sm z-10">
       <div className="basis-[24px] bg-gray-200 p-2 flex justify-center items-center rounded-sm ">
         <WarningIcon class="text-gray-600 fill-current" />
       </div>
       <div className="grow p-2 flex justify-start items-center">
-        <p className="text-xs">something terrible happened</p>
+        <p className="text-xs">{error}</p>
       </div>
       <div className="p-2 max-w-4 flex justify-center items-center">
         <button onClick={() => setError(null)}><CrossNoCircleIcon class="scale-50 text-gray-600 fill-current text-center" /></button>
