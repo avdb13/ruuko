d068e35f4a3b8bb1532cc9374e32bd4de89c513b
mikoto <avdb@keemail.me>
2023-12-09 10:54:24 +0000

    login box

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,8 @@ import { useCookies } from "react-cookie";
 import axios from "axios";
 import { parse, tldExists } from "tldjs";
 import matrix, { Credentials } from "../lib/matrix";
+import Arrow from "./icons/ArrowBox";
+import ArrowIcon from "./icons/Arrow";
 
 type baseUrlState = "disconnected" | "valid" | "invalid" | "loading";
 
@@ -14,31 +16,28 @@ const Login = () => {
   );
 
   return (
-    <div className="w-screen h-screen bg-[#222]">
-      <div>
-        <div
-          className="chip"
-          style={{
-            background: "linear-gradient(#855afc, #3ac8fc)",
-            filter: "hue-rotate(-15deg) blur(60px)",
-            translate: "10% 20%",
-            scale: "120%",
-            rotate: "45deg",
-            borderRadius: "30% 70% 70% 30% / 30% 30% 70% 70%",
-          }}
-        ></div>
-        <div
-          className="chip"
-          style={{
-            background: "linear-gradient(#855afc, #3ac8fc)",
-            filter: "hue-rotate(15deg) blur(60px)",
-            scale: "140%",
-            translate: "-10% -20%",
-            borderRadius: "30% 70% 70% 30% / 30% 30% 70% 70%",
-          }}
-        ></div>
-      </div>
-
+    <div className="h-max w-max bg-[#222] overflow-hidden">
+      <div
+        className="chip"
+        style={{
+          background: "linear-gradient(#855afc, #3ac8fc)",
+          filter: "hue-rotate(-15deg) blur(60px)",
+          translate: "10% 20%",
+          scale: "120%",
+          rotate: "45deg",
+          borderRadius: "30% 70% 70% 30% / 30% 30% 70% 70%",
+        }}
+      ></div>
+      <div
+        className="chip"
+        style={{
+          background: "linear-gradient(#855afc, #3ac8fc)",
+          filter: "hue-rotate(15deg) blur(60px)",
+          scale: "140%",
+          translate: "-10% -20%",
+          borderRadius: "30% 70% 70% 30% / 30% 30% 70% 70%",
+        }}
+      ></div>
       <div
         style={{
           position: "absolute",
@@ -87,9 +86,7 @@ const FinalForm = ({
             .then((session) => setCookie("session", session, { path: "/" }));
         }
       }}
-      className={`bg-transparent basis-72 flex flex-col justify-center items-center gap-1 h-screen w-screen opacity-0 -delay-500 duration-300 transition-all ${
-        credentials.baseUrl ? "-translate-x-[0px] opacity-100" : null
-      }`}
+    className="bg-transparent basis-72 flex flex-col justify-center items-center gap-1 h-screen w-screen opacity-0 -delay-500 duration-300 transition-all"
     >
       <div>
         <label className="px-2 w-full text-sm text-gray-600 font-semibold">
@@ -180,40 +177,22 @@ const ServerForm = ({
 
   return (
     <div
-      className={`flex justify-center items-center h-screen w-screen duration-300 ${
-        state === "valid"
-          ? "-translate-x-[200px] opacity-0 delay-1000"
-          : state === "invalid"
-          ? "animation-pulse"
-          : null
-      }`}
+      className="flex justify-center items-center h-screen w-screen duration-300"
     >
       <div
-        className={`relative basis-72 flex flex-col justify-center items-center gap-1`}
+        className="relative basis-80 border-b-4 border-gray-400 bg-blue-50 p-2 rounded-md shadow-xl flex justify-center gap-2"
       >
-        <label className="px-2 w-full text-sm text-gray-600 font-semibold">
-          server
-        </label>
         <input
-          className={`duration-300 placeholder:opacity-0 transition-all w-full basis-4 border-4 border-gray-400 focus:outline-none focus:border-4 focus:border-gray-600 rounded-md p-2 shadow-md ${
-            state === null
-              ? ""
-              : state === "valid"
-              ? "border-green-400"
-              : state === "invalid"
-              ? "border-red-400"
-              : "border-red-400 placeholder:opacity-100"
-          }`}
-          type="text"
+          placeholder="server"
+    className="placeholder:opacity-100 placeholder:font-semibold w-full border-4 border-gray-300 focus:border-gray-600 outline-none invalid:border-red-400 rounded-md p-2 shadow-md duration-300 transition-all"
+          type="url"
+          minLength={1}
           ref={baseUrlRef}
           value={baseUrl}
           onChange={(e) => setServer(e.target.value)}
         />
-        {state === "loading" ? (
-          <div className="absolute left-[90px] bottom-[57.5px] lds-ellipsis scale-75 opacity-50"></div>
-        ) : null}
-        <button onClick={testServer} className="invisible">
-          next
+        <button onClick={testServer} className="rounded-md basis-16 bg-white border-4 border-gray-300 shadow-md flex justify-center items-center">
+          <ArrowIcon className="opacity-50" />
         </button>
       </div>
     </div>
